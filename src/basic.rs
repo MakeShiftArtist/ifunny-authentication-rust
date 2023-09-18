5d8a5d46373ea0de1d1daf0d6f3b8395a3ad11f0
makeshiftartist <47653736+MakeShiftArtist@users.noreply.github.com>
2023-09-17 22:00:22 -0500

    refac(basic token): wrap string & and config opts
    - Convert BasicGenerator to BasicToken(String)
    - Add `new` method
    - Add configurable lengths

--- src/basic.rs
+++ src/basic.rs
@@ -1,39 +1,99 @@
-use rand::{thread_rng, Rng};
+use uuid::Uuid;
 
+use base64ct::{Base64, Encoding};
 use sha1_smol::Sha1;
+use sha2::{Digest, Sha256};
 
-use base64ct::{
-    Base64,
-    Encoding
-};
+#[derive(Debug)]
+pub struct BasicToken(String);
 
-pub struct BasicGenerator;
+impl std::fmt::Display for BasicToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
-impl BasicGenerator {
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BasicTokenLength {
+    Length112 = 112,
+    Length156 = 156,
+}
+
+impl BasicToken {
     pub const CLIENT_ID: &str = "MsOIJ39Q28";
     pub const CLIENT_SECRET: &str = "PTDc3H8a)Vi=UYap";
-    pub const CHARSET: &[u8] = b"ABCDEF1234567890";
 
-    /// Generate a new Basic Authorization token for iFunny.
-    /// 
-    /// ## Examples
+    /// Create a new Basic Authorization token for iFunny with the given client_id and client_secret and length
+    ///
+    /// # Arguments
+    /// * `client_id` - The client_id for iFunny
+    /// * `client_secret` - The client_secret for iFunny
+    /// * `length` - The length of the token (112 or 156)
+    ///
+    /// # Returns
+    /// * BasicToken
+    ///
+    /// # Examples
     /// ```
-    /// use lib::basic::BasicGenerator;
-    /// let basic = BasicGenerator::generate();
+    /// use ifunny_auth::basic::{BasicToken, BasicTokenLength};
+    ///
+    /// let basic: BasicToken = BasicToken::new("MsOIJ39Q28", "PTDc3H8a)Vi=UYap", BasicTokenLength::Length112);
+    /// assert_eq!(basic.len(), 112);
     /// ```
-    pub fn generate() -> String {
-        let mut rng = thread_rng();
-        let hex: String = (0..72).map(|_|{
-            unsafe {
-                char::from(*Self::CHARSET.get_unchecked(rng.gen_range(0..16)))
+    pub fn new(client_id: &str, client_secret: &str, length: BasicTokenLength) -> Self {
+        let uuid = Uuid::new_v4().simple();
+
+        let hex = match length {
+            BasicTokenLength::Length112 => uuid.to_string().to_uppercase(),
+            BasicTokenLength::Length156 => {
+                let mut hasher = Sha256::new();
+                hasher.update(uuid.to_string());
+
+                let result = hasher.finalize();
+                format!("{:x}", result)
             }
-        }).collect();
-        let hex_id = format!("{}_{}", &hex, BasicGenerator::CLIENT_ID);
-        let decoded_hash: String = format!("{}:{}:{}", &hex, BasicGenerator::CLIENT_ID, Self::CLIENT_SECRET);
-        let mut encoded_hash = Sha1::new();
-        encoded_hash.update(decoded_hash.as_bytes());
-        let encoded_hash: String = encoded_hash.digest().to_string();
-        let auth_token = format!("{}:{}", hex_id, encoded_hash);
-        Base64::encode_string(auth_token.as_bytes())
+        }
+        .to_uppercase();
+
+        let a = format!("{}_{}:", hex, client_id);
+        let b = format!("{}:{}:{}", hex, client_id, client_secret);
+
+        let c: String = {
+            let mut hasher = Sha1::new();
+            hasher.update(b.as_bytes());
+            format!("{}", hasher.digest().to_string())
+        };
+
+        let decoded_hash = format!("{}{}", a, c);
+
+        Self(Base64::encode_string(decoded_hash.as_bytes()))
+    }
+
+    /// Generate a new Basic Authorization token for iFunny using the default values for client_id and client_secret and length
+    ///
+    /// # Examples
+    /// ```
+    /// use ifunny_auth::basic::BasicToken;
+    /// let basic: BasicToken = BasicToken::generate();
+    /// ```
+    pub fn generate() -> Self {
+        Self::new(
+            BasicToken::CLIENT_ID,
+            BasicToken::CLIENT_SECRET,
+            BasicTokenLength::Length112,
+        )
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl From<String> for BasicToken {
+    /// Convert a String into a BasicToken
+    ///
+    /// ? This assumes the string is base64 encoded
+    fn from(s: String) -> Self {
+        Self(s)
     }
 }
